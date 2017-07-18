3dd042a775becdf11a854dacdb507ea9e14da7eb
Sung <sungmkim80@gmail.com>
2017-07-17 22:13:15 -0400

    able to get sentiment data

--- src/components/show_messages.js
+++ src/components/show_messages.js
@@ -1,17 +1,23 @@
 import _ from 'lodash';
 import React, {Component} from 'react';
+import sentiment from 'sentiment';
 
 export default class ShowMessages extends Component {
     getMessageJSX = ({messages}) => {
         console.log('ShowMessages.messages', messages);
-        if (_.isEmpty(messages)) return <div>loading...</div>;
+        if (_.isEmpty(messages)) return <div>Loading...</div>;
 
         let messageJSX = _.map(messages, message => {
-            if (message.subtype === "message_changed") {
-                return <li key={message.ts}>{message.message.text}</li>;
-            } else {
-                return <li key={message.ts}>{message.text}</li>;
-            }
+            let text = message.text;
+            if (message.previousMessage) text = message.message.text;
+
+            let sentimentResult = sentiment(text);
+            console.log(text);
+            console.group();
+            console.log(sentimentResult);
+            console.groupEnd();
+
+            return <li key={message.ts}>{text}</li>;
         });
 
         return messageJSX;
