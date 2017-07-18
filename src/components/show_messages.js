8e3f59e3445ce294ca388bae15298df3aa62f964
Sung <sungmkim80@gmail.com>
2017-07-18 09:31:27 -0400

    sets graph boundary

--- src/components/show_messages.js
+++ src/components/show_messages.js
@@ -17,14 +17,17 @@ export default class ShowMessages extends Component {
             },
         ];
 
+        let today = new Date();
+        let maxTime = new Date();
+        maxTime.setMinutes(today.getMinutes() + 10);
+
         this.state = {
-            messages: [],
-            rows: [
-                [new Date(1789, 4, 30), 0]
-            ],
+            // rows: [
+            //     [new Date(1789, 4, 30), 0]
+            // ],
             columns: [
                 {
-                    type: 'date',
+                    type: 'datetime',
                     label: 'Time',
                 },
                 {
@@ -34,11 +37,34 @@ export default class ShowMessages extends Component {
             ],
             options: {
                 title: 'Sentiment vs Time',
-                hAxis: {title: 'Time'},
-                vAxis: {title: 'Sentiment', minValue: -100, maxValue: 100},
+                hAxis: {
+                    title: 'Time',
+                    viewWindow: {
+                        min: today,
+                        max: maxTime,
+                    },
+                    gridlines: {
+                        count: -1,
+                        units: {
+                            days: {format: ['MMM dd']},
+                            hours: {format: ['HH:mm', 'ha']},
+                        }
+                    },
+                    minorGridlines: {
+                        units: {
+                            hours: {format: ['hh:mm:ss a', 'ha']},
+                            minutes: {format: ['HH:mm a Z', ':mm']}
+                        }
+                    }
+                },
+                vAxis: {title: 'Sentiment', minValue: -10, maxValue: 10},
                 legend: 'none',
             },
         };
+
+        this.rows = [
+            [new Date(), 0],
+        ];
 
         // https://medium.com/@justintulk/best-practices-for-resetting-an-es6-react-components-state-81c0c86df98d
         this.baseState = _.pickBy(this.state, (propName) => propName !== "messages");
@@ -51,29 +77,46 @@ export default class ShowMessages extends Component {
         console.log(this.props);
 
         const {message} = this.props;
-        this.setState({messages: [...this.state.messages, message]});
+        let text = message.text;
+        if (message.previousMessage) text = message.message.text;
 
-        this.setState(this.baseState);
-        console.log('after base state: this.state.messages.length', this.state.messages.length);
+        let sentimentResult = sentiment(text);
+        console.log(text);
+        console.group();
+        console.log(sentimentResult);
+        console.groupEnd();
 
-        _.map(this.state.messages, message => {
-            let text = message.text;
-            if (message.previousMessage) text = message.message.text;
+        const {score} = sentiment(text);
+        let date = new Date();
+        let newRow = [date, score];
 
-            let sentimentResult = sentiment(text);
-            console.log(text);
-            console.group();
-            console.log(sentimentResult);
-            console.groupEnd();
+        console.log(newRow);
 
-            const {score} = sentiment(text);
-            let date = new Date();
-            let newRow = [date, score];
+        // this.setState({rows: [...this.state.rows, newRow]});
+        this.rows = [...this.rows, newRow];
+        console.log(this.rows);
 
-            console.log(newRow);
-
-            this.setState({rows: [...this.state.rows, newRow]});
-        });
+        // // this.setState(this.baseState);
+        // // console.log('after base state: this.state.messages.length', this.state.messages.length);
+        //
+        // _.map(this.state.messages, message => {
+        //     let text = message.text;
+        //     if (message.previousMessage) text = message.message.text;
+        //
+        //     let sentimentResult = sentiment(text);
+        //     console.log(text);
+        //     console.group();
+        //     console.log(sentimentResult);
+        //     console.groupEnd();
+        //
+        //     const {score} = sentiment(text);
+        //     let date = new Date();
+        //     let newRow = [date, score];
+        //
+        //     console.log(newRow);
+        //
+        //     this.setState({rows: [...this.state.rows, newRow]});
+        // });
         //
         // // only update chart if the data has changed
         // if (prevProps.data !== this.props.data) {
@@ -110,7 +153,7 @@ export default class ShowMessages extends Component {
         return (
             <Chart
                 chartType="LineChart"
-                rows={this.state.rows}
+                rows={this.rows}
                 columns={this.state.columns}
                 options={this.state.options}
                 graph_id="ScatterChart"
